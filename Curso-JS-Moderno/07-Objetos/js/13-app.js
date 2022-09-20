006300cff340370eb50e5d9ffb1b193fec2774f2
Carlos Diaz <carlosdiaz.dev@gmail.com>
2022-09-20 01:08:17 -0500

    [ADD] Changes on html files to lection 12 | 08-Arrays -> 07-app.js -> UV-55

---
+++ Curso-JS-Moderno/07-Objetos/js/13-app.js
@@ -0,0 +1,11 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio:300,
+    disponible: true
+}
+
+console.log("Objeto Normal", producto)
+
+console.log("Keys", Object.keys(producto));
+console.log("Valores", Object.values(producto));
+console.log("Entradas", Object.entries(producto));
--- Curso-JS-Moderno/08-Arrays/js/07-app.js
+++ Curso-JS-Moderno/08-Arrays/js/07-app.js
@@ -9,22 +9,47 @@ const carrito = [];
 
 // Definir un producto
 const producto = {
-    nombre: "Monitor 32 pulgadas",
-    precio: 400
-}
+  nombre: "Monitor 32 pulgadas",
+  precio: 400,
+};
 const producto2 = {
-    nombre: "Celular",
-    precio: 800
-}
+  nombre: "Celular",
+  precio: 800,
+};
 
-carrito.push(producto2)
-carrito.push(producto)
+// Agrega elementos al final de un array
+carrito.push(producto);
+carrito.push(producto2);
 
 const producto3 = {
     nombre: "Teclado",
-    precio: 50
-}
+  precio: 50,
+};
 
-carrito.unshift(producto3)
+const producto4 = {
+    nombre: "Celular 2",
+    precio: 3000,
+};
 
+// Agrega elementos al final de un array
+carrito.push(producto3);
+carrito.push(producto4);
+
+
+/* // Agrga elementos al inicio de un array
+// carrito.unshift(producto3);
+console.table(carrito);
+
+// Eliminar ultimo elemento del arreglo
+carrito.pop();
+console.table(carrito);
+
+// Eliminar del inicio del arreglo
+carrito.shift();
+console.table(carrito); */
+
+
+console.table(carrito);
+
+carrito.splice(1,1)
 console.table(carrito);
